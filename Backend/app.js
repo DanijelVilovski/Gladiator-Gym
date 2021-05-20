b96b4b33d0bee4867d303994b9c1c8d92c1d677e
Danijel Vilovski <danivilovski@gmail.com>
2021-05-20 21:12:30 +0200

    20/05

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./database/models/user');
+const Membership = require('./database/models/membership');
+
+//express app
 const app = express();
 
-app.listen(3000, () => console.log("Server connected on port 3000"));
+//connect to mongodb
+const dbURI = 'mongodb+srv://danijel:test1234@cluster0.myqfw.mongodb.net/WEB_Projekat_BazaPodataka?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
+app.get('/add-user', (req, res) => {
+    const user = new User({
+        name: 'Danijel',
+        surname: 'Vilovski',
+        email: 'danivilovski@gmail.com',
+        password: 'password'
+    });
+    user.save()
+        .then((result) =>{
+            res.send(result)
+        })
+        .catch((err) =>{
+            console.log(err)
+        });
+})
+
+app.get('/', function (req, res) {
+    res.send('Ideeeee gas');
+})
